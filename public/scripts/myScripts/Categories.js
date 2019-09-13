f5d4e45d5cede4ba87d0f1924564f5c659036344
Dawood <dawood.aijaz97@gmail.com>
2019-09-14 02:13:16 +0500

    all backgrounds are black

--- public/scripts/myScripts/Categories.js
+++ public/scripts/myScripts/Categories.js
@@ -10,6 +10,5 @@ let categories = [];
 categories.push(new Categories("Photography", "Photography.jpg"));
 categories.push(new Categories("Light box", "LightBox.jpg"));
 categories.push(new Categories("Sculpture", "Sculpture.jpg"));
-categories.push(new Categories("Drawing", "Drawing.jpg"));
 categories.push(new Categories("Olfactory space design", "ARD.jpg"));
 categories.push(new Categories("mode", "ARD.jpg"));
